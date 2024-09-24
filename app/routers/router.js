236cbb3c61b64eab95c52164c50af99a50fd842b
Arun <arunsolisnavas@gmail.com>
2024-09-23 19:58:43 -0600

    router

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Importar controladores
 const cuentas = require('../controllers/cuenta.controller.js');
@@ -7,21 +7,20 @@ const tipoTransacciones = require('../controllers/tipoTransaccion.controller.js'
 const controlTransacciones = require('../controllers/controlTransaccion.controller.js');
 
 // Rutas para cuentas
-router.post('/api/cuentas/create', cuentas.create);
-router.get('/api/cuentas/all', cuentas.retrieveAllCuentas);
-router.get('/api/cuentas/:id', cuentas.getCuentaById);
-router.put('/api/cuentas/update/:id', cuentas.updateById);
-router.delete('/api/cuentas/delete/:id', cuentas.deleteById);
+router.post('/cuentas/create', cuentas.create);
+router.get('/cuentas/all', cuentas.retrieveAllCuentas);
+router.get('/cuentas/:id', cuentas.getCuentaById);
+router.put('/cuentas/update/:id', cuentas.updateById);
+router.delete('/cuentas/delete/:id', cuentas.deleteById);
 
 // Rutas para tipoTransacciones
-router.post('/api/tipoTransacciones/create', tipoTransacciones.create);
-router.get('/api/tipoTransacciones/all', tipoTransacciones.retrieveAllTipos);
-router.get('/api/tipoTransacciones/:id', tipoTransacciones.getTipoById);
+router.post('/tipoTransacciones/create', tipoTransacciones.create);
+router.get('/tipoTransacciones/all', tipoTransacciones.retrieveAllTipos);
+router.get('/tipoTransacciones/:id', tipoTransacciones.getTipoById);
 
 // Rutas para controlTransacciones
-router.post('/api/controlTransacciones/create', controlTransacciones.create);
-router.get('/api/controlTransacciones/all', controlTransacciones.retrieveAllTransacciones);
-router.get('/api/controlTransacciones/:id', controlTransacciones.getTransaccionById);
-
+router.post('/controlTransacciones/create', controlTransacciones.create);
+router.get('/controlTransacciones/all', controlTransacciones.retrieveAllTransacciones);
+router.get('/controlTransacciones/:id', controlTransacciones.getTransaccionById);
 
 module.exports = router;
